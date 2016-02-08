94bb27b6b531a46dae6a2eab68380825e1566be9
Mohammed Shajib <mohammed.shajib@f1006-01-34125.bmcc.cuny.edu>
2016-02-08 18:50:07 -0500

    javaScript

--- WEEK-2/classWork/js/main.js
+++ WEEK-2/classWork/js/main.js
@@ -1,8 +1,8 @@
 var submit = document.getElementById("submit");
 submit.onclick = function() {
     var name = document.getElementById("name").value;
-    var email = document.getElementById("emial").value;
+    var email = document.getElementById("email").value;
     var borough = document.getElementById("borough").value;
     var message = document.getElementById("message");
-    message.innerHTML = "Hello" + name + "from" + borough +"Welcome to my website";
+    message.innerHTML = "Hello" + name + ", from " + borough +", Welcome to my website";
 };
