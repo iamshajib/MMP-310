110b119f04aa0fe5db1f7a27463313ffcc3a9481
Mohammed Shajib <mohammed.shajib@f1006-01-34125.bmcc.cuny.edu>
2016-02-29 19:04:22 -0500

    magic 8 ball final

--- WEEK-4/magic8ball/js/main.js
+++ WEEK-4/magic8ball/js/main.js
@@ -1,7 +1,7 @@
 var answers = [
   'Maybe.', 'Certainly not.', 'I hope so.', 'Not in your wildest dreams.',
   'There is a good chance.', 'Quite likely.', 'I think so.', 'I hope not.',
-  'I hope so.', 'Never!', 'Fuhgeddaboudit.', 'Ahaha! Really?!?', 'Pfft.',
+  'I hope so.', 'Never!', 'Ahaha! Really?!?',
   'Sorry, bucko.', 'Hell, yes.', 'Hell to the no.', 'The future is bleak.',
   'The future is uncertain.', 'I would rather not say.', 'Who cares?',
   'Possibly.', 'Never, ever, ever.', 'There is a small chance.', 'Yes!'];
