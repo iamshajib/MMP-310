56e56954718be623ba5ade5331ae4d9fc0e26f29
Mohammed Shajib <mohammed.shajib@f1006-01-34125.bmcc.cuny.edu>
2016-03-08 21:03:47 -0500

    Assignment 5: Gameshow Quiz

--- WEEK-5/Homework-5/js/main.js
+++ WEEK-5/Homework-5/js/main.js
@@ -1,29 +1,51 @@
 document.getElementById("sub").onclick = function(){
-    console.log(document.getElementsByName("class")[0].checked);
-    console.log(document.getElementsByName("class")[1].checked);
 
     if (document.getElementById("yes").checked) {
         document.getElementById("answer").innerHTML = "True";
     }
     else{
-        document.getElementById("answer").innerHTML = "False";
+        document.getElementById("answer").innerHTML = "False. Talk to your advisor.";
     }
 }
 
 document.getElementById("sub2").onclick = function(){
 
-//    console.log(document.getElementsByName("sky")[0].checked);
-//    console.log(document.getElementsByName("sky")[1].checked);
-
-    console.log(document.getElementsByName("sky")[0].value);
-    console.log(document.getElementsByName("sky")[1].value);
-
-    if (document.getElementById("no").checked) {
-        document.getElementById("answer").innerHTML = "True";
+    if (document.getElementById("yes2").checked) {
+        document.getElementById("answer2").innerHTML = "False. Whats the color of the dress?";
     }
     else{
-        document.getElementById("answer").innerHTML = "False";
+        document.getElementById("answer2").innerHTML = "True";
     }
 }
 
 
+document.getElementById("sub3").onclick = function(){
+
+    if (document.getElementById("yes3").checked) {
+        document.getElementById("answer3").innerHTML = "False. I am a computer.";
+    }
+    else{
+        document.getElementById("answer3").innerHTML = "True";
+    }
+}
+
+
+document.getElementById("sub4").onclick = function(){
+
+    if (document.getElementById("yes4").checked) {
+        document.getElementById("answer4").innerHTML = "True";
+    }
+    else{
+        document.getElementById("answer4").innerHTML = "Go back to school.";
+    }
+}
+
+document.getElementById("sub5").onclick = function(){
+
+    if (document.getElementById("yes5").checked) {
+        document.getElementById("answer5").innerHTML = "True";
+    }
+    else{
+        document.getElementById("answer5").innerHTML = "You need to study physiology.";
+    }
+}
