9a4e68c6041f39ab2230daefe1311ac21dbe9df5
Mohammed Shajib <mohammed.shajib@f1006-01-34125.bmcc.cuny.edu>
2016-03-15 19:50:30 -0400

    assignment-6

--- WEEK-6/assignment-6/js/main.js
+++ WEEK-6/assignment-6/js/main.js
@@ -1,6 +1,51 @@
-var submit = document.getElementById("submit");
-submit.onclick = function() {
-    var name = document.getElementById("food").value;
-    var message = document.getElementById("message");
-    message.innerHTML = "Hello " + name + " welcome.";
-};
+function get_menu() {
+    var menu = new Array;
+    var menu_str = localStorage.getItem('todo');
+    if (menu_str !== null) {
+        menu = JSON.parse(menu_str);
+    }
+    return menu;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+
+    var menu = get_menu();
+    menu.push(task);
+    localStorage.setItem('todo', JSON.stringify(menu));
+
+    show();
+
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var menu = get_menu();
+    menu.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(menu));
+
+    show();
+
+    return false;
+}
+
+function show() {
+    var menu = get_menu();
+
+    var html = '<ul>';
+    for(var i=0; i<menu.length; i++) {
+        html += '<li>' + menu[i] + '<button class="remove" id="' + i  + '">x</button></li>';
+    };
+    html += '</ul>';
+
+    document.getElementById('menu').innerHTML = html;
+
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
