2e34e60ee00127629ab1de523467c13b7de6ffe5
Mohammed Shajib <mohammed.shajib@f1006-01-34125.bmcc.cuny.edu>
2016-03-16 18:06:58 -0400

    week-7

---
+++ WEEK-7/inClasss/js/main.js
@@ -0,0 +1,12 @@
+var questions = ["Is the sky blue?",
+                "What is the capital of Bangladesh?",
+                "What is 2+ 2?"];
+
+var answer = [
+                "yes",
+                "Dhaka",
+                "4"];
+
+var q = document.createElement("p");
+q.innerHTML = "Is the sky blue?";
+document.body.appendChild(q);
