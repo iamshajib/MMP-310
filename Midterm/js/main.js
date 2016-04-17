1fd93281243d38b9b57a79dcb1cbf58f0cda1eb7
iamshajib <iamsajib1@gmail.com>
2016-04-17 09:46:53 -0700

    edit

--- Midterm/js/main.js
+++ Midterm/js/main.js
@@ -1,5 +1,7 @@
-var questAns = [
+var qa = [
     ["How many days in a leep year?", "366" ],
     ["The world smallest country is?", "Vatican City" ],
-    ["The brighest planet is?", "Venus"]
+    ["The brighest planet is?", "Venus"],
+    ["Country that was called as Land of Rising Sun?", "Japan"],
+    ["Where is the biggest museum located?", "London"]
 ]
