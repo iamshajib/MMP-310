e044e65fa18dca9ee69d851d183ed1d9e4491b88
Mohammed Shajib <mohammed.shajib@f1006-01-34125.bmcc.cuny.edu>
2016-05-09 19:08:11 -0400

    class work

--- Final/pacman/js/game.js
+++ Final/pacman/js/game.js
@@ -1,47 +1,58 @@
-var player;
-var speed = 0.1;
-var jump = 20;
-var jumping = false;
-
-var gravity = 1;
-
-var platform;
+var pacman, ghost;
+var pacman_walk_sprites, pacman_walk, pacman_stand_sprites, pacman_stand;
 var obstacle;
 
+function preload() {
+    pacman_walk_sprites = loadSpriteSheet("image/pacman.png", 50, 50, 6);
+    pacman_walk = loadAnimation(pacman_walk_sprites);
+    //    pacman_stand_sprites = loadSpriteSheet("image/pacman.png", 50, 50, 1);
+    //    pacman_stand = loadAnimation(pacman_stand_sprites);
+
+}
+
 function setup() {
-	createCanvas( 720, 360 );
-	player = createSprite( 400, 100, 40, 40 );
-	platform = createSprite( width/2, height, width, 40);
-	obstacle = createSprite( 300, height, 50, 100);
+    createCanvas(900, 500);
+
+
+    pacman = createSprite(70, 40);
+    pacman.addAnimation("walk", pacman_walk);
+    //pacman.addAnimation("stand", pacman_stand);
+    pacman.setCollider("circle", 0, 0, 32, 32);
+    //player.debug = true;
+
+
+    obstacle = createSprite(300, 200);
+    obstacle.debug = true;
+    obstacle.addImage(loadImage("image/circle.png"));
+    obstacle.setCollider("circle", 0, 0, 18, 18);
+
+
+    ghost = createSprite(100, 100);
+    ghost.addImage(loadImage("image/ghost.png"));
 }
 
 function draw() {
-	background("black");
-
-	if ( keyDown( "a" ) ) {
-		player.velocity.x -= speed;
-	} else if ( keyDown( "d" ) ) {
-		player.velocity.x += speed;
-	}
+    background("black");
 
 
+    pacman.collide(obstacle, function() {
+        obstacle.remove();
+    });
 
+    drawSprites();
+}
 
-	player.velocity.y += gravity;
-	if ( player.collide( platform ) ) {
-		player.velocity.y = 0;
-		if (jumping) jumping = false;
-	}
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        pacman.setSpeed(2, 180);
+        pacman.mirrorX(-1);
+    } else if (keyCode === RIGHT_ARROW) {
+        pacman.setSpeed(2, 0);
+        pacman.mirrorX(1);
+    } else if (keyCode === UP_ARROW) {
+        pacman.setSpeed(2, 270);
+    } else if (keyCode === DOWN_ARROW) {
+        pacman.setSpeed(2, 90);
+    }
 
-	if ( keyDown( "space" ) && !jumping ) {
-		player.velocity.y -= jump;
-		jumping = true;
-	}
-
-	if ( player.collide( obstacle ) ) {
-		player.remove();
-
-	}
-
-	drawSprites();
 }
