044aa5790306faac88b23c95e1006906191859fb
iamshajib <Mflsssbus12d>
2016-05-19 19:02:25 -0700

    edit

--- Midterm/js/main.js
+++ Midterm/js/main.js
@@ -1,5 +1,5 @@
 function getAnswers() {
-    var total = 5;
+    var total = 10;
     var score = 0;
 
     var q1 = document.forms["question"]["q1"].value;
@@ -7,6 +7,11 @@ function getAnswers() {
     var q3 = document.forms["question"]["q3"].value;
     var q4 = document.forms["question"]["q4"].value;
     var q5 = document.forms["question"]["q5"].value;
+    var q6 = document.forms["question"]["q6"].value;
+    var q7 = document.forms["question"]["q7"].value;
+    var q8 = document.forms["question"]["q8"].value;
+    var q9 = document.forms["question"]["q9"].value;
+    var q10 = document.forms["question"]["q10"].value;
 
     for (i = 1; i <= total; i++) {
         if (eval('q' + i) === null || eval('q' + i) === '') {
@@ -15,7 +20,7 @@ function getAnswers() {
         }
     }
 
-    var answers = ["c", "d", "b", "a", "b"];
+    var answers = ["c", "d", "b", "a", "b", "D", "B", "C", "B", "A"];
 
     for (i = 1; i <= total; i++) {
         if (eval('q' + i) === answers[i - 1]) {
